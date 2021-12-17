9e77c7933bc13dbc9491953b7354c336cb9ed698
DaymianDark <nightkonn@gmail.com>
2021-12-17 13:26:38 +0300

    changed tests

--- src/test/java/gmail/com/d/berezkin42/PracticeFormTests.java
+++ src/test/java/gmail/com/d/berezkin42/PracticeFormTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -24,13 +26,14 @@ public class PracticeFormTests {
         $("#firstName").setValue("Vasiliy"); //firstName
         $("#lastName").setValue("Pupkin"); //lastName
         $("#userEmail").setValue("vasiliy.p@email.com"); //userEmail
-
-        //$(By.name("gender")).selectRadio("Male");
-        //$("#gender-radio-1").click(); //gender-radio-1
-
+        $("label.custom-control-label").shouldHave(text("Male")).click();
+        //$(".custom-control-label").shouldHave(text("Male")).click();
+        //$(byText("Male")).click();
+        //$(".custom-control-label").click(); //gender-radio-1
         $("#userNumber").setValue("4951234567"); //userNumber
 
-        $("#dateOfBirthInput").setValue("12 Apr 1961"); //dateOfBirthInput 12 Apr 1961
+        $("#dateOfBirthInput").sendKeys(Keys.CONTROL+"a");
+        $("#dateOfBirthInput").sendKeys("12 Apr 1961"); //dateOfBirthInput 12 Apr 1961
 
         $("#subjectsInput").setValue("Subject text"); //subjectsInput
 
